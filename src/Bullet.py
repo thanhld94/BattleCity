0e20ff4c15ccf0f0e3208921d062dbed6ebbd6f0
thanhld94 <thanhld94@gmail.com>
2016-10-27 21:11:31 -0500

    Modified starting position in bullet

--- src/Bullet.py
+++ src/Bullet.py
@@ -1,13 +1,14 @@
 class Bullet:
 
   def __init__(self, row, col, direction):
-    self.row = row
-    self.col = col
-    self.direction = direction
     # up, down, left, right
     global dx, dy
     dx = [1, -1, 0, 0]
     dy = [0, 0, 1, -1]
+    self.direction = direction
+    self.row = row + dx[direction]
+    self.col = col + dy[direction]
+    
 
   def move(self):
     self.row += dx[self.direction]
