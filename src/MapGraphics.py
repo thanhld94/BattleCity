91661b161bc157e7a22a71987d0ba744f69b510d
lendkhoa <khoadang.le@outlook.com>
2016-10-30 13:39:37 -0500

    Fix player and enemy location bugs and add legend

--- src/MapGraphics.py
+++ src/MapGraphics.py
@@ -20,12 +20,42 @@ class MapGraphics:
         #create window
         win = GraphWin()
         win.setCoords(0,0,30,30)
+        # Legend information
+        # -Water
+        water = Rectangle (Point(22, 0), Point(24, 2))
+        water.draw(win)
+        water.setFill("blue")
+        waterSign = Text(Point(25,1), "Water")
+        waterSign.draw(win)
+        # - Brick
+        brick = Rectangle (Point(22, 2), Point(24, 4))
+        brick.draw(win)
+        brick.setFill("brown")
+        brickSign = Text(Point(25,3), "Brick")
+        brickSign.draw(win)
+        # - Base
+        base = Rectangle (Point(22, 4), Point(24, 6))
+        base.draw(win)
+        base.setFill("yellow")
+        baseSign = Text(Point(25,5), "Base")
+        baseSign.draw(win)
+        # Delete this polygon if use other map than the 10x10 matrix
         p = Polygon(Point(0,0), Point(0, 20), Point(20,20), Point(20,0))
         p.setWidth(4)
         p.draw(win)
         #draw map
         for row in range (0, len(self.env.board)*2, 2):
             for col in range (0, len(self.env.board[0])*2, 2):
+                # locate the position of enemy
+                if (enemy.pos_row == int(row/2) and enemy.pos_col == int(col/2)):
+                  rec = Rectangle(Point(row, col), Point(row+2, col+2));
+                  rec.draw(win)
+                  rec.setFill("black")
+                # position of player
+                if (p1.pos_row == int(row/2) and p1.pos_col == int(col/2)):
+                  rec = Rectangle(Point(row, col), Point(row+2, col+2));
+                  rec.draw(win)
+                  rec.setFill("green")
                 # if the cell is empty
                 if (self.env.board[int(row/2)][int(col/2)] == 0):
                   rec = Rectangle(Point(row, col), Point(row+2, col+2));
@@ -44,10 +74,6 @@ class MapGraphics:
                   rec = Rectangle(Point(row, col), Point(row+2, col+2));
                   rec.draw(win)
                   rec.setFill("yellow")
-                elif (enemy.pos_row == int(row/2) and enemy.pos_col == int(col/2)):
-                  rec = Rectangle(Point(row, col), Point(row+2, col+2));
-                  rec.draw(win)
-                  rec.setFill("red")
 
 
 
@@ -57,7 +83,7 @@ bullets_1 = []
 bullets_2 = []
 p1 = Enemy(env, bullets_1) 
 enemy = Enemy(env, bullets_2)
-p1.setup(2,2,enemy)
+p1.setup(2,7,enemy)
 enemy.setup(2,2, p1)
 battleGround = MapGraphics(env, enemy, p1)
 battleGround.drawMap()
