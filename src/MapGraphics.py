d0284c23c387e63168a6b28cf33a6a81d1992ad0
onimushachip <nguyenlam141@yahoo.com>
2016-11-01 23:59:59 -0500

    Working graphic map with bullets, fixed slowdown bug

--- src/MapGraphics.py
+++ src/MapGraphics.py
@@ -3,6 +3,7 @@
     in the Environment.py
 '''
 import time, os, sys
+
 from third_parties.zellegraphics import *
 from Environment import *
 from Enemy import *
@@ -17,8 +18,8 @@ class MapGraphics:
         self.enemy = enemy
         self.p1 = player
         #create window
-        self.win = GraphWin()
-        self.win.setCoords(0,0,30,30)
+        self.win = GraphWin("Graphics", 600, 600)
+        self.win.setCoords(0,0,20,20)
         # Delete this polygon if use other map than the 10x10 matrix
         p = Polygon(Point(0,0), Point(0, 20), Point(20,20), Point(20,0))
         p.setWidth(1)
@@ -29,12 +30,16 @@ class MapGraphics:
         self.prev_p1_pos = []
         self.prev_p1_pos.append(self.p1.pos_row)
         self.prev_p1_pos.append(self.p1.pos_col)
-                
+        self.prev_p1_bullets = []
+        self.prev_enemy_bullets = []
+
     def update(self, environment, enemy, player):
         self.prev_enemy_pos[0]= self.enemy.pos_row
         self.prev_enemy_pos[1]= self.enemy.pos_col
         self.prev_p1_pos[0]= self.p1.pos_row
         self.prev_p1_pos[1]= self.p1.pos_col
+        self.prev_p1_bullets = self.p1.bullets
+        self.prev_enemy_bullets = self.enemy.bullets
         self.env = environment
         self.enemy = enemy
         self.p1 = player
@@ -43,24 +48,9 @@ class MapGraphics:
     # Check each cell of the matrix and redraw and change color of each cell base
     # on the difference between old and new update environment
     # Future test: did not test the case in which the brick cell is delete because of bullet collision
-    def drawMap(self):
-        self.win.update()
-        # delete old position of enemy and player
-        self.matrix[self.prev_enemy_pos[0]*2][self.prev_enemy_pos[1]*2].setFill("white")
-        self.matrix[self.prev_p1_pos[0]*2][self.prev_p1_pos[1]*2].setFill("white")
+    def drawInitialMap(self):
         for row in range (0, len(self.env.board)*2, 2):
             for col in range (0, len(self.env.board[0])*2, 2):
-                # locate the position of enemy
-                if (self.enemy.pos_row == int(row/2) and self.enemy.pos_col == int(col/2)):
-                   self.matrix[row][col] = Rectangle(Point(row, col), Point(row+2, col+2))
-                   self.matrix[row][col].draw(self.win)
-                   self.matrix[row][col].setFill("black")
-                # position of player
-                if (self.p1.pos_row == int(row/2) and self.p1.pos_col == int(col/2)):
-                   self.matrix[row][col] = Rectangle(Point(row, col), Point(row+2, col+2))
-                   self.matrix[row][col].draw(self.win)
-                   self.matrix[row][col].setFill("green")
-                # if the cell is empty
                 if (self.env.board[int(row/2)][int(col/2)] == 0):
                    self.matrix[row][col] = Rectangle(Point(row, col), Point(row+2, col+2))
                    self.matrix[row][col].draw(self.win)
@@ -79,18 +69,32 @@ class MapGraphics:
                    self.matrix[row][col].draw(self.win)
                    self.matrix[row][col].setFill("yellow")
 
+    def drawMap(self):
+        self.win.update()
+        # delete old position of enemy and player
+        self.matrix[self.prev_enemy_pos[0]*2][self.prev_enemy_pos[1]*2].setFill("white")
+        self.matrix[self.prev_p1_pos[0]*2][self.prev_p1_pos[1]*2].setFill("white")
+        # delete old positions of bullets
+        for row in range (0, len(self.env.board)*2, 2):
+            for col in range (0, len(self.env.board[0])*2, 2):
+                    for bullets in self.prev_p1_bullets:
+                        if (bullets.row == int(row / 2) and bullets.col == int(col / 2)):
+                            print("Found a prev bullet")
+                            self.matrix[row][col].setFill("white")
 
-#test
-#env = Environment()
-#bullets_1 = []
-#bullets_2 = []
-#p1 = Enemy(env, bullets_1) 
-#enemy = Enemy(env, bullets_2)
-#p1.setup(2,7,enemy)
-#enemy.setup(2,2, p1)
-#battleGround = MapGraphics(env, enemy, p1)
-#i = 10
-#while (i>=0):
-#    battleGround.drawMap()
-#    i = i - 1
-#battleGround.drawMap()
+        for row in range (0, len(self.env.board)*2, 2):
+            for col in range (0, len(self.env.board[0])*2, 2):
+                # locate the position of enemy
+                if (self.enemy.pos_row == int(row/2) and self.enemy.pos_col == int(col/2)):
+                   self.matrix[row][col].setFill("black")
+                # position of player
+                if (self.p1.pos_row == int(row/2) and self.p1.pos_col == int(col/2)):
+                   self.matrix[row][col].setFill("green")
+                # locate player bullets
+                for bullets in self.p1.bullets:
+                    if (bullets.row == int(row/2) and bullets.col == int(col/2)):
+                        self.matrix[row][col].setFill("grey")
+                # locate enemies' bullets
+                for bullets in self.enemy.bullets:
+                    if (bullets.row == int(row/2) and bullets.col == int(col/2)):
+                        self.matrix[row][col].setFill("cyan")
