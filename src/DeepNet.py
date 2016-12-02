2a6bae109d1ec4c43e6b6b1335934f9d244caab5
thanhld94 <thanhld94@gmail.com>
2016-12-01 19:53:35 -0600

    Reorganize and comments - Thanh Le

--- src/DeepNet.py
+++ src/DeepNet.py
@@ -42,17 +42,15 @@ y_ = tf.placeholder('float')
 # creating the model for the neural network
 # 3 hidden layer with an output layer with each node represent a possible action
 # including move up/down/left/right and shoot
-hidden_1_layer = {'weights':tf.Variable(tf.random_normal([numberOfFeatures, nodesLayer1])), 
-                    'biases':tf.Variable(tf.random_normal([nodesLayer1]))}
-hidden_2_layer = {'weights':tf.Variable(tf.random_normal([nodesLayer1, nodesLayer2])), 
-                    'biases':tf.Variable(tf.random_normal([nodesLayer2]))}
-hidden_3_layer = {'weights':tf.Variable(tf.random_normal([nodesLayer2, nodesLayer3])), 
-                    'biases':tf.Variable(tf.random_normal([nodesLayer3]))}
-output_layer = {'weights':tf.Variable(tf.random_normal([nodesLayer3, numberOfActions])), 
-                  'biases':tf.Variable(tf.random_normal([numberOfActions])),}
-
-# Calculating the output
 def neural_network_model(data):
+    hidden_1_layer = {'weights':tf.Variable(tf.random_normal([numberOfFeatures, nodesLayer1])), 
+                      'biases':tf.Variable(tf.random_normal([nodesLayer1]))}
+  hidden_2_layer = {'weights':tf.Variable(tf.random_normal([nodesLayer1, nodesLayer2])), 
+                      'biases':tf.Variable(tf.random_normal([nodesLayer2]))}
+  hidden_3_layer = {'weights':tf.Variable(tf.random_normal([nodesLayer2, nodesLayer3])), 
+                      'biases':tf.Variable(tf.random_normal([nodesLayer3]))}
+  output_layer = {'weights':tf.Variable(tf.random_normal([nodesLayer3, numberOfActions])), 
+                    'biases':tf.Variable(tf.random_normal([numberOfActions])),}
   l1 = tf.add(tf.matmul(data,hidden_1_layer['weights']), hidden_1_layer['biases'])
   l1 = tf.nn.relu(l1)
   l2 = tf.add(tf.matmul(l1,hidden_2_layer['weights']), hidden_2_layer['biases'])
